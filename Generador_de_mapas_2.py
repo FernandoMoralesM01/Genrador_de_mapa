d625d8588b8f8d4c37a50045e53e7856d851a242
FernandoMoralesM01 <fernandomoralesm01@gmail.com>
2022-06-11 22:05:46 -0500

    Cambios en los popups y otros cambios menores.
    Se realizó una implementación de código en html para desplegar de manera ordenada la información de los popups.
    Se añadió el string <batallon> para wue sea considerado consultorio.
    Se actualizó el punto de origen del mapa.

--- Generador_de_mapas_2.py
+++ Generador_de_mapas_2.py
@@ -1,3 +1,4 @@
+from turtle import up
 import pandas as pd
 import numpy as np
 import folium
@@ -17,13 +18,13 @@ def Validar_tipo(Lat, Lon, Tipologia, Nombre):
     Nombre = str(Nombre)
     Nombre = Nombre.lower()
 
-    if (Nombre.count('hospital') > 0 or Tipologia.count('hospital') > 0 or Nombre.count('centro') > 0):
+    if (Nombre.count('hospital') > 0 or Tipologia.count('hospital') > 0 or Nombre.count('centro') > 0 ):
         tipo = 'HOSPITAL'
     if (Nombre.count('cl') > 0 or Tipologia.count('cl') > 0):
         tipo = 'CLINICA'
     if(Nombre.count('sana') > 0):
         tipo = 'SANATORIO'
-    if(Nombre.count('consultorio') > 0):
+    if(Nombre.count('consultorio') > 0 or Nombre.count('batallon') > 0):
         tipo = 'CONSULTORIO'
 
     
@@ -56,8 +57,11 @@ def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
     Nombres_tec = np.array(Nombres_tec)
     
     Arr_Tec = pd.DataFrame(tecnologias, columns= ['Hospital / Unidad Médica', 'Equipo', 'Descripción', 'Marca', 'Modelo', 'Cantidad'])
+    #Arr_tec                                               [0]                   [1]        [2]          [3]       [4]        [5]
     Arr_Tec = np.array(Arr_Tec)
     html = ""
+    upperHtml = "<html>  <head>     <style>     {         box-sizing: content-box;     }     /* Set additional styling options for the columns*/     .column {     float: left;     width: 1%;     }      .row:after {     content: "";     display: table;     clear: both;     }     </style>  </head>  <body>"
+    html += '<br class="row">' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'EQUIPO' +' </h4> </div>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'MARCA' +' </h4> </div>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'MODELO' +' </h4> </div>' + '</br></br></br></div>'
     siDatos = 0
     i = 0
     for aString in Nombres_tec:
@@ -82,20 +86,35 @@ def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
         if (aString.count(nombre) > 0 or countPalabra >= 5):
             #print(nombre, aString)
             siDatos = 1
+            equipo = str(Arr_Tec[i][1])
+            marca =  str(Arr_Tec[i][3])
+            modelo = str(Arr_Tec[i][4])
+            cantidad = str(Arr_Tec[i][5])
+
             
-            html +=  str(Arr_Tec[i][1]) + " - " + str(Arr_Tec[i][5]) +"<br>"
-             
+            equipo = '<div class="column" style="background-color:#FFFFFF;"> <p>' + cantidad + '&nbsp;-&nbsp;'+equipo +' </p> </div>'
+            marca = '<div class="column" style="background-color:#FFFFFF;"> <p>' + marca +' </p> </div>'
+            modelo = '<div class="column" style="background-color:#FFFFFF;"> <p>' + modelo +' </p> </div>'
+            
+            
+            html += '<br class="row">' + equipo + marca + modelo + '</br></br></br></div>'
+
+            
+        
         i += 1
 
     if(siDatos == 0):
-        html = "No hay datos"
+        #html = '<html>  <head>     <style>     {         box-sizing: border-box;     }     /* Set additional styling options for the columns*/     .column {     float: left;     width: 1%;     }      .row:after {     content: "";     display: table;     clear: both;     }     </style>  </head>  <body>     <div class="row">         <div class="column" style="background-color:#FFB695;">             <h2>Column 1</h2>             <p>Data..</p>         </div>         <div class="column" style="background-color:#96D1CD;">             <h2>Column 2</h2>             <p>Data..</p>         </div>     </div>      <div class="row">         <div class="column" style="background-color:#FFB695;">             <h2>Column 1</h2>             <p>Data..</p>         </div>         <div class="column" style="background-color:#96D1CD;">             <h2>Column 2</h2>             <p>Data..</p>         </div>     </div>  </body> </html>'
+        html = 'No hay datos'
         iframe = folium.IFrame(html,
                         width=125,
                         height=50)
 
     else:
+        
+        html = upperHtml + html + '  </body> </html>'
         iframe = folium.IFrame(html,
-                        width=300,
+                        width=13000,
                         height=200)
     
     return [iframe, 400, siDatos]
@@ -130,7 +149,7 @@ def Generar_marcadores (hospital, tecnologias, Mapa):
     Cords = pd.DataFrame(hospital, columns= ['LATITUD', 'LONGITUD'])
     Cords = np.array(Cords)
     
-    Mapa.add_child(plugins.HeatMap(Cords[0:len(Cords)]))
+    #Mapa.add_child(plugins.HeatMap(Cords[0:len(Cords)]))
 
     Nombres = pd.DataFrame(hospital, columns= ['NOMBRE DE LA UNIDAD'])
     Nombres = np.array(Nombres)
@@ -149,25 +168,27 @@ def Generar_mapa_con_marcadores(N_arch):
     N_arch = (N_arch[1: (len(N_arch) - 1)])
     print(N_arch)
 
-    Mapa = folium.Map(zoom_start= 12, location=[19.4385309, -99.2088913])   #Genera un Mapa nuevo
+    Mapa = folium.Map(zoom_start= 12, location=[19.419886, -99.298375])   #Genera un Mapa nuevo
     
     hospital = pd.read_excel (N_arch, sheet_name= 'HOSPITAL')
     tecnologias = pd.read_excel (N_arch, sheet_name= 'TECNOLOGÍAS MÉDICAS')
 
     Generar_marcadores (hospital, tecnologias, Mapa)
     
+    N_arch = N_arch[0: len(N_arch) - 5]
     Mapa.save(N_arch + 'Map.html')  #Guarda el mapa en la dirección el excel
 
 def Generar_mapa_de_calor(N_arch):
     N_arch = (N_arch[1: (len(N_arch) - 1)])
     print(N_arch)
-    Mapa = folium.Map(zoom_start= 12, location=[19.4385309, -99.2088913])   #Genera un Mapa nuevo
+    Mapa = folium.Map(zoom_start= 12, location=[19.419886, -99.298375])   #Genera un Mapa nuevo
     hospital = pd.read_excel (N_arch, sheet_name= 'HOSPITAL')
     Cords = pd.DataFrame(hospital, columns= ['LATITUD', 'LONGITUD'])
     Cords = np.array(Cords)
-
+    print(Cords)
     Mapa.add_child(plugins.HeatMap(Cords[0:len(Cords)]))
 
+    N_arch = N_arch[0: len(N_arch) - 5]
     Mapa.save(N_arch + 'HeatMap.html')  #Guarda el mapa en la dirección el excel
     return 0
 
@@ -175,3 +196,5 @@ def Generar_mapa_de_calor(N_arch):
 N_arch =Ingresa_nombre_de_archivo()
 Generar_mapa_con_marcadores(N_arch)
 Generar_mapa_de_calor(N_arch)
+
+#"C:\Users\fercy\Desktop\proyekta medica\PM - Hadassah_Hospital Data.xlsx"
