32cfb0655653703b9530e21aa25cc5e7baa01263
FernandoMoralesM01 <fernandomoralesm01@gmail.com>
2022-06-12 19:49:45 -0500

    Datos de descripción.
    Se agrego el dato de la distancia a la descripción de cada maracador.
    Se agregó un marcador con la localización central del mapa.

--- Generador_de_mapas_2.py
+++ Generador_de_mapas_2.py
@@ -1,4 +1,4 @@
-from turtle import up
+import geopy.distance
 import pandas as pd
 import numpy as np
 import folium
@@ -10,7 +10,7 @@ def Ingresa_nombre_de_archivo():
 
 def Validar_tipo(Lat, Lon, Tipologia, Nombre):   
     esValido = True
-    tipo = 'NOSE'
+    tipo = 'No especificado'
 
     Tipologia = str(Tipologia)
     Tipologia = Tipologia.lower()
@@ -18,13 +18,13 @@ def Validar_tipo(Lat, Lon, Tipologia, Nombre):
     Nombre = str(Nombre)
     Nombre = Nombre.lower()
 
-    if (Nombre.count('hospital') > 0 or Tipologia.count('hospital') > 0 or Nombre.count('centro') > 0 ):
+    if (Nombre.count('hospital') > 0 or Tipologia.count('hospital') > 0 or Nombre.count('cent') > 0 or Nombre.count('hg') > 0 or  Nombre.count('h. g. ') > 0):
         tipo = 'HOSPITAL'
     if (Nombre.count('cl') > 0 or Tipologia.count('cl') > 0):
         tipo = 'CLINICA'
     if(Nombre.count('sana') > 0):
         tipo = 'SANATORIO'
-    if(Nombre.count('consultorio') > 0 or Nombre.count('batallon') > 0):
+    if(Nombre.count('consultorio') > 0 or Nombre.count('bata') > 0):
         tipo = 'CONSULTORIO'
 
     
@@ -50,17 +50,30 @@ def Validar_tipo(Lat, Lon, Tipologia, Nombre):
     return [esValido, icon, icon_color, Lat, Lon]
 
 def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
+    Lat = float(Lat)
+    Lon = float(Lon)
+    coords1 = str(Lat) + ", " + str(Lon)
+    coords2 = "19.419886, -99.298375"
+    dist = float (geopy.distance.geodesic(coords1, coords2).km)
+    dist = round(dist, 3)
+
     nombre = str(nombre)
     nombre = nombre.lower()
 
     Nombres_tec = pd.DataFrame(tecnologias, columns= ['Hospital / Unidad Médica'])
     Nombres_tec = np.array(Nombres_tec)
     
-    Arr_Tec = pd.DataFrame(tecnologias, columns= ['Hospital / Unidad Médica', 'Equipo', 'Descripción', 'Marca', 'Modelo', 'Cantidad'])
-    #Arr_tec                                               [0]                   [1]        [2]          [3]       [4]        [5]
+
+    Arr_Tec = pd.DataFrame(tecnologias, columns= ['Hospital / Unidad Médica', 'Equipo', 'Descripción', 'Marca', 'Modelo', 'Cantidad', 'Latitude', 'Longitude'])
+    #Arr_tec                                               [0]                   [1]        [2]          [3]       [4]        [5]         [6]        [7]
     Arr_Tec = np.array(Arr_Tec)
+    
+    siCoincide = False
+    Ncoinc = 5
     html = ""
+    
     upperHtml = "<html>  <head>     <style>     {         box-sizing: content-box;     }     /* Set additional styling options for the columns*/     .column {     float: left;     width: 1%;     }      .row:after {     content: "";     display: table;     clear: both;     }     </style>  </head>  <body>"
+    html += '<br class="row">' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'Distancia' +' </h4> <p>' + str(dist) + ' km </p> </div>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'Tiempo' +' </h4> </div> </br></br></br></br></br></br></div>'
     html += '<br class="row">' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'EQUIPO' +' </h4> </div>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'MARCA' +' </h4> </div>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'MODELO' +' </h4> </div>' + '</br></br></br></div>'
     siDatos = 0
     i = 0
@@ -71,6 +84,7 @@ def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
 
         palabras = aString.split(" /")
         aString = ""
+        
         for palabra in palabras:
             aString += palabra
 
@@ -81,9 +95,11 @@ def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
         for palabra in palabras:
             if(nombre.count(palabra) > 0 and nombre.count(palabra) < 2):
                 countPalabra += 1
-        
-        
-        if (aString.count(nombre) > 0 or countPalabra >= 5):
+        #print("...", aString)
+        if(aString.count(" ") < 4):
+            Ncoinc = 4
+            
+        if (aString.count(nombre) > 0 or countPalabra >= Ncoinc or siCoincide == True):
             #print(nombre, aString)
             siDatos = 1
             equipo = str(Arr_Tec[i][1])
@@ -105,19 +121,19 @@ def  agregar_descripcion(nombre, Lat, Lon, tecnologias):
 
     if(siDatos == 0):
         #html = '<html>  <head>     <style>     {         box-sizing: border-box;     }     /* Set additional styling options for the columns*/     .column {     float: left;     width: 1%;     }      .row:after {     content: "";     display: table;     clear: both;     }     </style>  </head>  <body>     <div class="row">         <div class="column" style="background-color:#FFB695;">             <h2>Column 1</h2>             <p>Data..</p>         </div>         <div class="column" style="background-color:#96D1CD;">             <h2>Column 2</h2>             <p>Data..</p>         </div>     </div>      <div class="row">         <div class="column" style="background-color:#FFB695;">             <h2>Column 1</h2>             <p>Data..</p>         </div>         <div class="column" style="background-color:#96D1CD;">             <h2>Column 2</h2>             <p>Data..</p>         </div>     </div>  </body> </html>'
-        html = 'No hay datos'
+        html = upperHtml + '<br class="row">' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'Distancia' +' </h4> <p>' + str(dist) + ' km </p> </div> </br></br></br></br></br></br>' + '<div class="column" style="background-color:#FFFFFF;"> <h4>'+ 'Tiempo' +' </h4> </div> </br></br></br></br></br></br></div>' + '  </body> </html>'
         iframe = folium.IFrame(html,
-                        width=125,
-                        height=50)
+                        width= 200,
+                        height=150)
 
     else:
         
         html = upperHtml + html + '  </body> </html>'
         iframe = folium.IFrame(html,
-                        width=13000,
-                        height=200)
+                        width=14000,
+                        height=300)
     
-    return [iframe, 400, siDatos]
+    return [iframe, 450, siDatos]
 
 
 def Insertar_marcador(Cords, Tipologia, Nombre, tecnologias, Mapa):
@@ -170,13 +186,19 @@ def Generar_mapa_con_marcadores(N_arch):
 
     Mapa = folium.Map(zoom_start= 12, location=[19.419886, -99.298375])   #Genera un Mapa nuevo
     
+    folium.Marker(location=[19.419886, -99.298375], 
+                icon= folium.Icon(icon= "hospital-o", prefix= 'fa', color= "orange"),
+                tooltip= "TERRENO HADASAH", 
+                popup=folium.Popup("Bosque Real Huixquilucan de Degollado, State of Mexico", max_width= 200)).add_to(Mapa)
+
+
     hospital = pd.read_excel (N_arch, sheet_name= 'HOSPITAL')
     tecnologias = pd.read_excel (N_arch, sheet_name= 'TECNOLOGÍAS MÉDICAS')
 
     Generar_marcadores (hospital, tecnologias, Mapa)
     
     N_arch = N_arch[0: len(N_arch) - 5]
-    Mapa.save(N_arch + 'Map.html')  #Guarda el mapa en la dirección el excel
+    Mapa.save(N_arch + '_Map.html')  #Guarda el mapa en la dirección el excel
 
 def Generar_mapa_de_calor(N_arch):
     N_arch = (N_arch[1: (len(N_arch) - 1)])
@@ -189,7 +211,7 @@ def Generar_mapa_de_calor(N_arch):
     Mapa.add_child(plugins.HeatMap(Cords[0:len(Cords)]))
 
     N_arch = N_arch[0: len(N_arch) - 5]
-    Mapa.save(N_arch + 'HeatMap.html')  #Guarda el mapa en la dirección el excel
+    Mapa.save(N_arch + '_HeatMap.html')  #Guarda el mapa en la dirección el excel
     return 0
 
 #MAIN
